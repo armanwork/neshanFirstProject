5ede580d4cc3ffca0b0fe6e2126ea7d853853578
armannajafib <armannajafi81@gmail.com>
2024-07-29 13:57:25 +0430

    add space

--- app/src/main/java/com/example/firstprojectbuild/MainActivity.java
+++ app/src/main/java/com/example/firstprojectbuild/MainActivity.java
@@ -14,15 +14,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int integer = 56;
 
 
 
 
 
 
-
-
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
