861aac08663e9256bc71608a291958a56b30de2e
armannajafib <armannajafi81@gmail.com>
2024-07-29 14:01:12 +0430

    integer deleted

--- app/src/main/java/com/example/firstprojectbuild/MainActivity.java
+++ app/src/main/java/com/example/firstprojectbuild/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -14,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int integer = 56;
+        int arman = 34;
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
