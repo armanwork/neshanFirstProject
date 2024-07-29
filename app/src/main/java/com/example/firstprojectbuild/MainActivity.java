af0b0c15b1d53c77c975130da576c4b699848a14
armannajafib <armannajafi81@gmail.com>
2024-07-30 01:39:19 +0430

    delete last comment

--- app/src/main/java/com/example/firstprojectbuild/MainActivity.java
+++ app/src/main/java/com/example/firstprojectbuild/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
 
 
-            //armannnn work       test
+            //delete last comment
 
         });
     }
